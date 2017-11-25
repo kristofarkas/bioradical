7996d9f390b3c945d7eac558262d057effef08c6
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2017-11-25 19:06:32 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bioradical',
+
+    version='0.1',
+
+    description='Bioradical',
+
+    long_description='Copy from README file',
+
+    url='http://ccs.chem.ucl.ac.uk',
+
+    author='CCS',
+
+    packages=find_packages()
+)
+
