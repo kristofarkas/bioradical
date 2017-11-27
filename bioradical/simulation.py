0d2c1a1c1dfc65d049f4130379c7163f0519f8e0
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2017-11-27 18:30:01 +0000

    Change executable

--- bioradical/simulation.py
+++ bioradical/simulation.py
@@ -1,7 +1,7 @@
 from radical.entk import Task, Stage
 
 _simulation_file_suffixes = ['.coor', '.xsc', '.vel']
-_namd = '/u/sciteam/jphillip/NAMD_build.latest/NAMD_2.12_CRAY-XE-ugni-BlueWaters-memopt/namd2'
+_namd = '/u/sciteam/jphillip/NAMD_LATEST_CRAY-XE-MPI-BlueWaters/namd2'
 # _namd = 'namd2'
 
 
